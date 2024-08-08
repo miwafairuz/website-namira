5540332a8000e257f9badc560a6d4dd72ad95081
miwafairuz <namirafwib@gmail.com>
2024-08-08 19:03:25 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav ul li a');
+
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            document.querySelector(link.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
